e495f28b995336b856a00a77d149b20d8abd342d
childishbeat <14wardn@wh-at.net>
2022-08-05 13:36:45 +0100

    Pure Python implementation

--- autotrim.py
+++ autotrim.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import binascii
 a=sys.argv[1]
 b=2
 with open(a,'rb')as f:content=f.read().hex()
@@ -7,6 +8,4 @@ while True:
     if content[-b]!=content[-2]and content[-b-1]!=content[-1]:break
     b=b+2
 os.rename(a,a+'.bak')
-with open(a+'.tmp','w')as f:f.write(content[:len(content)-len(content[-b+2:])])
-os.system('certutil -decodehex "'+a+'.tmp" "'+a+'"')
-os.remove(a+'.tmp')
+with open(a,'wb')as f:f.write(binascii.unhexlify(content[:len(content)-len(content[-b+2:])]))
